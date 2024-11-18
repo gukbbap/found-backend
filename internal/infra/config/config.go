93f82a33a1b049de89a7e5f2550df36cc4cda8fd
fullgukbap <2hanjunbum6@gmail.com>
2024-11-18 15:35:35 +0900

    feat: config 파일 추가

---
+++ internal/infra/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	Database struct {
+		User     string
+		Password string
+		Host     string
+		Port     string
+		Schema   string
+	}
+}
+
+func NewConfig(path string) (*Config, error) {
+	var cfg Config
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	err = toml.NewDecoder(file).Decode(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
