cf5d943a98ae66193f7449d722c85f0ef4b18f05
fullgukbap <2hanjunbum6@gmail.com>
2024-11-18 15:35:54 +0900

    feat: 테스트 완료

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,5 +1,16 @@
-package app
+package main
+
+import (
+	"fmt"
+	"found-backend/internal/infra/config"
+	"log"
+)
 
 func main() {
+	found, err := config.NewConfig(".env.toml")
+	if err != nil {
+		log.Panicf("failed to new config: %v\n", err)
+	}
 
+	fmt.Println("config struct: ", found)
 }
