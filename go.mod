8f49c729cd670ab32142610cc131ef565a65df23
fullgukbap <2hanjunbum6@gmail.com>
2024-11-19 12:20:22 +0900

    feat: userHandler 형태만 구현

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module found-backend
 go 1.23.2
 
 require (
+	github.com/labstack/echo/v4 v4.12.0
 	github.com/pelletier/go-toml/v2 v2.2.3
 	go.uber.org/fx v1.23.0
 	gorm.io/driver/mysql v1.5.7
@@ -14,10 +15,17 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/dig v1.18.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- internal/application/handler/handler_user.go
+++ internal/application/handler/handler_user.go
@@ -1,11 +1,51 @@
 package handler
 
-import "found-backend/internal/application/service"
+import (
+	"found-backend/internal/application/service"
+
+	"github.com/labstack/echo/v4"
+)
 
 type UserHandler struct {
 	userService *service.UserService
 }
 
 func NewUserHandler(userService *service.UserService) *UserHandler {
-	return &UserHandler{}
+	return &UserHandler{
+		userService: userService,
+	}
+}
+
+func (h UserHandler) Route(g *echo.Group) {
+	g.POST("/", h.CreateUser)
+	g.GET("/:id", h.FindUser)
+	g.PATCH("/:id", h.UpdateUser)
+	g.DELETE("/:id", h.DeleteUser)
+
+	g.GET("/:id/letters", h.GetLetters)
+	g.GET("/:id/feelings", h.GetFeelings)
+}
+
+func (h UserHandler) CreateUser(c echo.Context) error {
+	return nil
+}
+
+func (h UserHandler) FindUser(c echo.Context) error {
+	return nil
+}
+
+func (h UserHandler) UpdateUser(e echo.Context) error {
+	return nil
+}
+
+func (h UserHandler) DeleteUser(e echo.Context) error {
+	return nil
+}
+
+func (h UserHandler) GetLetters(e echo.Context) error {
+	return nil
+}
+
+func (h UserHandler) GetFeelings(e echo.Context) error {
+	return nil
 }
