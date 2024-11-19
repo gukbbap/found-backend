f122982586283180b2e7c94154be44bf958183f2
fullgukbap <2hanjunbum6@gmail.com>
2024-11-19 12:36:25 +0900

    feat: echo를 이용해 route 설정

--- internal/router.go
+++ internal/router.go
@@ -2,32 +2,49 @@ package router
 
 import (
 	"context"
-	"fmt"
+	"found-backend/internal/application/handler"
 	"found-backend/internal/infra/config"
 
+	"github.com/labstack/echo/v4"
 	"go.uber.org/fx"
 )
 
 type Router struct {
 	config *config.Config
+
+	// handlers
+	userHandler *handler.UserHandler
+
+	// router
+	engine *echo.Echo
 }
 
 func NewRouter(
 	lc fx.Lifecycle,
-	config *config.Config,
 
+	config *config.Config,
+	userHandler *handler.UserHandler,
 ) *Router {
 	// init struct
 	r := Router{
 		config: config,
+
+		// handlers
+		userHandler: userHandler,
+
+		// engine
+		engine: echo.New(),
 	}
 
 	lc.Append(
 		fx.Hook{
 			OnStart: func(ctx context.Context) error {
 				go func() {
-					fmt.Println("Hi, it's on start hook of fx")
+					if err := r.Listen(r.config.Port); err != nil {
+						panic(err)
+					}
 				}()
+
 				return nil
 			},
 			OnStop: func(ctx context.Context) error {
@@ -37,4 +54,21 @@ func NewRouter(
 	)
 
 	return &r
+}
+
+func (r *Router) route() {
+	api := r.engine.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			users := v1.Group("/users")
+			{
+				r.userHandler.Route(users)
+			}
+		}
+	}
+}
+
+func (r *Router) Listen(addr string) error {
+	return r.engine.Start(addr)
 }
