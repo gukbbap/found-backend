796100a63804412b6069189d54790b6b852c5c5d
fullgukbap <2hanjunbum6@gmail.com>
2024-11-19 17:22:44 +0900

    feat: letter service 구현

--- internal/application/service/service_letter.go
+++ internal/application/service/service_letter.go
@@ -1,6 +1,10 @@
 package service
 
-import "found-backend/internal/application/repository"
+import (
+	"context"
+	"found-backend/internal/application/entity"
+	"found-backend/internal/application/repository"
+)
 
 type LetterService struct {
 	letterRepository *repository.LetterRepository
@@ -10,4 +14,49 @@ func NewLetterSerivce(letterRepository *repository.LetterRepository) *LetterServ
 	return &LetterService{
 		letterRepository: letterRepository,
 	}
+}
+
+func (s LetterService) CreateLetter(ctx context.Context, letter *entity.Letter) (*entity.Letter, error) {
+	createdLetter, err := s.letterRepository.CreateLetter(ctx, letter)
+	if err != nil {
+		return nil, err
+	}
+
+	return createdLetter, nil
+}
+
+func (s LetterService) FindLetter(ctx context.Context, id int) (*entity.Letter, error) {
+	foundLetter, err := s.letterRepository.FindLetter(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return foundLetter, nil
+}
+
+func (s LetterService) UpdateLetter(ctx context.Context, letter *entity.Letter) (*entity.Letter, error) {
+	updatedLetter, err := s.letterRepository.UpdateLetter(ctx, letter)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedLetter, err
+}
+
+func (s LetterService) DeleteLetter(ctx context.Context, id int) error {
+	err := s.letterRepository.DeleteLetter(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s LetterService) GetFeelings(ctx context.Context, userID int) ([]entity.Feeling, error) {
+	gotFeelings, err := s.letterRepository.GetFeelings(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return gotFeelings, nil
 }
