4c197969727cc8621a8b45190715aead88c806e6
fullgukbap <2hanjunbum6@gmail.com>
2024-11-19 17:27:24 +0900

    feat: feeling service 구현 완료

--- internal/application/service/service_feeling.go
+++ internal/application/service/service_feeling.go
@@ -1,6 +1,10 @@
 package service
 
-import "found-backend/internal/application/repository"
+import (
+	"context"
+	"found-backend/internal/application/entity"
+	"found-backend/internal/application/repository"
+)
 
 type FeelingService struct {
 	feelingRepository *repository.FeelingRepository
@@ -10,4 +14,40 @@ func NewFeelingService(feelingRepository *repository.FeelingRepository) *Feeling
 	return &FeelingService{
 		feelingRepository: feelingRepository,
 	}
+}
+
+func (s FeelingService) CreateFeeling(ctx context.Context, feeling *entity.Feeling) (*entity.Feeling, error) {
+	createdFeeling, err := s.feelingRepository.CreateFeeling(ctx, feeling)
+	if err != nil {
+		return nil, err
+	}
+
+	return createdFeeling, nil
+}
+
+func (s FeelingService) FindFeeling(ctx context.Context, id int) (*entity.Feeling, error) {
+	foundFeeling, err := s.feelingRepository.FindFeeling(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return foundFeeling, nil
+}
+
+func (s FeelingService) UpdateFeeling(ctx context.Context, feeling *entity.Feeling) (*entity.Feeling, error) {
+	updatedFeeling, err := s.feelingRepository.UpdateFeeling(ctx, feeling)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedFeeling, nil
+}
+
+func (s FeelingService) DeleteFeeling(ctx context.Context, id int) error {
+	err := s.feelingRepository.DeleteFeeling(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
